421d5814ef04edaa2c2f8d992eb805b755006d9b
ThomasRub <81524133+ThomasRub@users.noreply.github.com>
2026-05-05 11:31:36 +0200

    ajout d'argument
    ajout de l'argument assetName à la ligne de commande de create_asset pour passer le nom de l'asset au script create_asset.py

--- command_launcher.py
+++ command_launcher.py
@@ -21,14 +21,18 @@ hython_path = jsonPath["software"]["hython"]
 ########################################################## SET FUNCTIONS #############################################################
 ######################################################################################################################################
 
-def create_asset():
+def create_asset(asset_name):
     # lance le script create_asset.py avec hython dans powershell
+    # asset name : nom de l'asset à traiter, il est passé par la suite dans la commande  vers hython
     python_file_path = project_path + "/08_dev/hython/create_asset.py"
 
 
+    asset_name = str(asset_name)
     to_hython_path = "cd \'" + hython_path.replace("/hython.exe", "") + "\'"
-    print(subprocess.Popen("powershell.exe " + to_hython_path + " ; ./hython.exe \'" + python_file_path + "\'"))
+    print(subprocess.Popen("powershell.exe " + to_hython_path + " ; ./hython.exe \'" + python_file_path + "\' --assetName '" + asset_name + "\'"))
 
 ######################################################################################################################################
 ########################################################## CALL FUNCTIONS ############################################################
 ######################################################################################################################################
+
+# create_asset("table")
