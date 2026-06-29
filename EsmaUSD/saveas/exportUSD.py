77afe1dbe2a0b25fc3d6fa26d1f06363e4157fdc
ESCOURBANIES Noa <98655548+RonoBlag@users.noreply.github.com>
2026-06-29 15:42:51 +0200

    dept name changed

--- EsmaUSD/saveas/exportUSD.py
+++ EsmaUSD/saveas/exportUSD.py
@@ -12,7 +12,7 @@ def export_usd():
     if info is None:
         return
 
-    entity = info["entity"]          # dict Prism attendu par generateProductPath
+    entity = info["entity"]
     etype = entity["type"]
     dept = info["department"]
     task = info["task"]
@@ -24,11 +24,9 @@ def export_usd():
     path = core.products.generateProductPath(entity=entity, task=task, extension=".usda", version=None, location="global")
     master_Path = core.products.generateProductPath(entity=entity, task=task, extension=".usda", version="master", location="global")
 
-    if etype == "asset" and dept == "mod":
+    if etype == "asset" and dept == "02_mod":
         write_usd("mod", path, default_prim=info["name"], selection_only=True)
         create_master(path, master_Path, default_prim=info["name"])
-
-
     else:
         return
     
