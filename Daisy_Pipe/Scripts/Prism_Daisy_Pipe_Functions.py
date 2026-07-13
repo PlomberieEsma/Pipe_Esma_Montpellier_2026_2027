babbdfced2b2649ef9773e84af70c0b374fbc015
leelootrinh <leeloott@proton.me>
2026-07-13 12:49:18 +0200

    Amelioration asset browser ui with tasks declencheur

--- Daisy_Pipe/Scripts/Prism_Daisy_Pipe_Functions.py
+++ Daisy_Pipe/Scripts/Prism_Daisy_Pipe_Functions.py
@@ -35,44 +35,10 @@ from functools import partial
 import os, sys
 
 from DaisyTools.core.command_launcher import Command_launcher
+from DaisyTools.core.asset_browser import AssetBrowserUI
 
 from PrismUtils.Decorators import err_catcher_plugin as err_catcher
 
-class SelectedAssetsList(QTreeWidget):
-
-    #-----------------------------------------------------------------------------------#
-    # Class used in the Asset Browser to handle the asset movement between lists        #
-    # Actions to Drag and Drop the Asset from the origin list to the selected list      #
-    # Action to Delete the Asset from the selected list                                 #
-    #-----------------------------------------------------------------------------------#
-
-    
-    def __init__(self, parentDlg):
-        super(SelectedAssetsList, self).__init__()
-        self.parentDlg = parentDlg
-        self.setColumnCount(1)
-        self.setHeaderHidden(True)
-        self.setAcceptDrops(True)
-        self.setDragDropMode(QAbstractItemView.DropOnly)
-
-    def dragEnterEvent(self, event):
-        event.acceptProposedAction()
-
-    def dragMoveEvent(self, event):
-        event.acceptProposedAction()
-
-    def dropEvent(self, event):
-        event.acceptProposedAction()
-        try:
-            self.parentDlg.onAssetsDropped()
-        except Exception as e:
-            self.parentDlg.core.popup("Erreur drop: %s" % e)
-    
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Delete:
-            self.parentDlg.onRemoveSelectedAssets(self.selectedItems())
-        else:
-            super(SelectedAssetsList, self).keyPressEvent(event)
 
 class Prism_Daisy_Pipe_Functions(object):
     def __init__(self, core, plugin):
@@ -82,10 +48,13 @@ class Prism_Daisy_Pipe_Functions(object):
         if self.isStandalone():
             self.importUsdPackages()
 
+        self.Command_launcher = Command_launcher(core, plugin)
+        self.AssetBrowserUI = AssetBrowserUI(core, plugin)
         self.core.registerCallback("onProjectBrowserStartup", self.onProjectBrowserStartup, plugin=self)
         self.core.registerCallback("openPBAssetContextMenu", self.onOpenPBAssetContextMenu, plugin=self)
         self.core.registerCallback("openPBAssetTaskContextMenu", self.onOpenPBAssetTaskContextMenu, plugin=self)
-        self.Command_launcher = Command_launcher(core, plugin)
+        self.core.registerCallback("openPBShotTaskContextMenu", self.onOpenPBShotTaskContextMenu, plugin=self)
+
 
         self.core.registerCallback("onStateStartup", self.onStateStartup, plugin=self, priority=40)
         self.core.registerCallback("onStateGetSettings", self.onStateGetSettings, plugin=self)
@@ -99,160 +68,68 @@ class Prism_Daisy_Pipe_Functions(object):
         #-----------------------------------------------------------------------------------#
         # Create a general menu 'DaisyMenu'                                                 #
         # Create an option 'Asset Browser'                                                  #
-        # Launch the opening of the Asset Browser when 'Asset BRowser' is triggered         #
+        # Launch the opening of the Asset Browser when 'Asset Browser' is triggered         #
         #-----------------------------------------------------------------------------------#
 
         origin.daisyMenu = QMenu("DaisyMenu")
-        origin.daisyMenu.addAction("Asset Browser", partial(self.onAssetBrowserTriggered, origin))
+        # onAssetBrowserAction = QAction( "Asset Browser", origin)
+        # onAssetBrowserAction.triggered.connect(lambda: self.onAssetBrowser(origin))
+        # origin.daisyMenu.addAction(onAssetBrowserAction)
         origin.menubar.addMenu(origin.daisyMenu)
 
-    def onAssetBrowserTriggered(self, origin, checked=False):
+
+    ##############################################################################################################
+    ###########################     SHOT TASK Contextual Menu - Asset Browser     ################################
+    ##############################################################################################################
+
+    def onOpenPBShotTaskContextMenu(self, origin, rcMenu, widget):
         
         #-----------------------------------------------------------------------------------#
-        # Window Asset Browser to select assets to load into a scene during layout          #
+        # Create an option "Asset Browser" on the context menu in shots'task                #
+        # Only for SetDress and RLO Tasks                                                   #
+        # When clicked, launch the fAsset Browser UI with the task as argument              #
         #-----------------------------------------------------------------------------------#
+        
+        entity = origin.getCurrentEntity()
+        widgetType = "department" if widget == origin.lw_departments else "task"
 
-        try:
-            allAssetPaths = self.core.entities.getAssetPaths()
-            self.assetRoot = os.path.commonpath(allAssetPaths) if allAssetPaths else ""
+        if entity or entity["type"] in ["shot", "sequence"] and widgetType == "task":
             
-            self.assetBrowserDlg = QDialog(origin)
-            self.core.parentWindow(self.assetBrowserDlg, parent=origin)
-            self.assetBrowserDlg.setWindowTitle("Asset Browser")
-            self.assetBrowserDlg.resize(700, 600)
+            # Check the department
+            deptItem = origin.lw_departments.currentItem()
+            if not deptItem:
+                return
+            department = deptItem.data(Qt.UserRole)
 
-            mainLayout = QVBoxLayout(self.assetBrowserDlg)
-            columnsLayout = QHBoxLayout()
-            mainLayout.addLayout(columnsLayout)
+            # Check existing tasks and their names
+            existingTasks = self.core.entities.getCategories(entity, step=department)
+            # self.core.popup("Tasks trouvées: %s" % existingTasks)
 
-            # LEFT COLUMN : Prism Asset List
-            import EntityWidget
-            self.w_entities = EntityWidget.EntityWidget(core=self.core, refresh=True)
-            # Keep only necessary and hide shots
-            self.w_entities.tb_entities.setVisible(False)
-            self.w_entities.getPage("Assets").tw_tree.setDragEnabled(True)
-            self.selectedAssetsData = {}  # clé = asset_path (unique), valeur = entity dict
-            columnsLayout.addWidget(self.w_entities)
+            if "SetDress" in existingTasks:
+                openAssetBrowserAction = QAction( "AssetBrowser", origin)
+                openAssetBrowserAction.triggered.connect(lambda: self.onAssetBrowser(origin, task="SetDress"))
+                rcMenu.addAction(openAssetBrowserAction)
+            elif "RLO" in existingTasks:
+                openAssetBrowserAction = QAction( "AssetBrowser", origin)
+                openAssetBrowserAction.triggered.connect(lambda: self.onAssetBrowser(origin, task="RLO"))
+                rcMenu.addAction(openAssetBrowserAction)
+            else:
+                return        
 
-            # RIGHT COLUMN : Selected Assets
-            self.gb_selectedAssets = QGroupBox("Selected Assets")
-            lo_selectedAssets = QVBoxLayout()
-            self.gb_selectedAssets.setLayout(lo_selectedAssets)
-            self.lw_selectedAssets = SelectedAssetsList(self)
-            lo_selectedAssets.addWidget(self.lw_selectedAssets)
-            columnsLayout.addWidget(self.gb_selectedAssets)
-
-            # BOTTOM : Validate Button
-            self.btn_validate = QPushButton("Validate")
-            self.btn_validate.clicked.connect(self.onValidateAssetsBrowser)
-            mainLayout.addWidget(self.btn_validate)
-
-            self.assetBrowserDlg.show()
-        except Exception as e:
-            self.core.popup("Erreur AssetBrowser: %s" % e)
-
-    def onAssetsDropped(self):
+    def onAssetBrowser(self, origin, task=None):
         
         #-----------------------------------------------------------------------------------#
-        # Add asset to the 'Selected Assets' list then refresh list                         #
+        # Get the selected asset from the Create USD Asset option                           #
+        # Launch the create asset function from Toto's script                               #
         #-----------------------------------------------------------------------------------#
 
-        entities = self.w_entities.getCurrentData(returnOne=False)
-        entities = [e for e in entities if e["type"] == "asset"]
-
-        for entity in entities:
-            key = entity.get("asset_path")
-            if key and key not in self.selectedAssetsData:
-                self.selectedAssetsData[key] = entity
-
-        self.refreshSelectedAssetsList()
-
-    def refreshSelectedAssetsList(self):
-         
-        #-----------------------------------------------------------------------------------#
-        # Refresh the 'Selected Assets' list                                                #
-        #-----------------------------------------------------------------------------------#
-
-        self.lw_selectedAssets.clear()
-        self.lw_selectedAssets.setIconSize(QSize(50, 50))
-
-        assets = list(self.selectedAssetsData.values())
-        if not assets:
-            return
-
-        nodes = {}
-        for entity in assets:
-            relPath = entity.get("asset_path", "")
-            parts = relPath.replace("\\", "/").split("/")
-
-            parent = self.lw_selectedAssets
-            currentPath = ""
-            for i, part in enumerate(parts):
-                currentPath = os.path.join(currentPath, part)
-                if currentPath not in nodes:
-                    if parent is self.lw_selectedAssets:
-                        node = QTreeWidgetItem(self.lw_selectedAssets, [part])
-                    else:
-                        node = QTreeWidgetItem(parent, [part])
-                    nodes[currentPath] = node
-                else:
-                    node = nodes[currentPath]
-
-                if i == len(parts) - 1:
-                    node.setData(0, Qt.UserRole, entity)
-                    pm = self.core.entities.getEntityPreview(entity)
-                    if not pm:
-                        pm = self.core.media.emptyPrvPixmap
-                    node.setIcon(0, QIcon(pm))
-
-                parent = node
-
-        self.lw_selectedAssets.expandAll()
-
-    def onRemoveSelectedAssets(self, items):
-         
-        #-----------------------------------------------------------------------------------#
-        # Delete asset to the 'Selected Assets' list then refresh list                      #
-        #-----------------------------------------------------------------------------------#
-
-        for item in items:
-            entity = item.data(0, Qt.UserRole)
-            if not entity:
-                continue  # c'est un dossier, pas un asset - on ignore
-            key = entity.get("asset_path")
-            if key in self.selectedAssetsData:
-                del self.selectedAssetsData[key]
-        self.refreshSelectedAssetsList()
-
-    def onValidateAssetsBrowser(self):
-         
-        #-----------------------------------------------------------------------------------------#
-        # Output the list of selected asset in the Asset Browser with their path as a dictionary  #
-        #-----------------------------------------------------------------------------------------#
-
-        output = {}
-        for entity in self.selectedAssetsData.values():
-            name = entity.get("asset", "")
-            path = entity.get("paths", "")
-            output[name] = path
-        return output
-
-
-
-
-
-
-
-
-
-
-
-
+        self.AssetBrowserUI.onAssetBrowserTriggered(origin, task)
 
 
     ##############################################################################################################
     ###########################     ASSET Contextual Menu - Create USD      ######################################
     ##############################################################################################################
+
     def onOpenPBAssetContextMenu(self, origin, rcMenu, asset):
         
         #-----------------------------------------------------------------------------------#
@@ -265,7 +142,7 @@ class Prism_Daisy_Pipe_Functions(object):
         item = asset.data(Qt.UserRole)
         if item is None:
             return
-        self.core.popup("Item: %s" % item)
+        # self.core.popup("Item: %s" % item)
 
         # Check if the item is an asset
         if item["type"] != "asset":
@@ -290,6 +167,7 @@ class Prism_Daisy_Pipe_Functions(object):
     ##############################################################################################################
     ###########################     ASSET TASK Contextual Menu - Variant     #####################################
     ##############################################################################################################
+
     def onOpenPBAssetTaskContextMenu(self, origin, rcMenu, widget):
         
         #-----------------------------------------------------------------------------------#
@@ -299,40 +177,37 @@ class Prism_Daisy_Pipe_Functions(object):
         #-----------------------------------------------------------------------------------#
         
         # Asset is a PySide6.QtCore.QModelIndex
-        #Check where the cursor is to launch at the right spot
+        # Check where the cursor is to launch at the right spot
         entity = origin.getCurrentEntity()
-        if not entity or entity["type"] not in ["asset"]:
-            return
         widgetType = "department" if widget == origin.lw_departments else "task"
-        if widgetType != "task":
-            return
-        
-        #Check the department
-        deptItem = origin.lw_departments.currentItem()
-        if not deptItem:
-            return
-        department = deptItem.data(Qt.UserRole)
 
-        #Check existing tasks and their names
-        existingTasks = self.core.entities.getCategories(entity, step=department)
-        existingBaseTasks = [t for t in existingTasks if not any(c.isdigit() for c in t)]
-        #CHECK - self.core.popup("Tasks trouvées: %s" % existingBaseTasks)
+        if entity or entity["type"] in ["asset"] and widgetType == "task":
+            # Check the department
+            deptItem = origin.lw_departments.currentItem()
+            if not deptItem:
+                return
+            department = deptItem.data(Qt.UserRole)
 
-        # Create the contextual menu and actions
-        addVarMenu = QMenu("Add Variants", origin)
+            # Check existing tasks and their names
+            existingTasks = self.core.entities.getCategories(entity, step=department)
+            existingBaseTasks = [t for t in existingTasks if not any(c.isdigit() for c in t)]
+            # CHECK - self.core.popup("Tasks trouvées: %s" % existingBaseTasks)
 
-        if not existingBaseTasks:
-            emptyAction = QAction("No existing tasks", addVarMenu)
-            emptyAction.setEnabled(False)
-            addVarMenu.addAction(emptyAction)
-        else:
-            for taskName in existingBaseTasks:
-                taskAction = QAction("Add Variant : %s" % taskName, addVarMenu)
-                taskAction.triggered.connect(partial(self.onCreateVariant, origin, entity, department, taskName, existingTasks))
-                addVarMenu.addAction(taskAction)
+            # Create the contextual menu and actions
+            addVarMenu = QMenu("Add Variants", origin)
 
-        rcMenu.addMenu(addVarMenu)
-   
+            if not existingBaseTasks:
+                emptyAction = QAction("No existing tasks", addVarMenu)
+                emptyAction.setEnabled(False)
+                addVarMenu.addAction(emptyAction)
+            else:
+                for taskName in existingBaseTasks:
+                    taskAction = QAction("Add Variant : %s" % taskName, addVarMenu)
+                    taskAction.triggered.connect(partial(self.onCreateVariant, origin, entity, department, taskName, existingTasks))
+                    addVarMenu.addAction(taskAction)
+
+            rcMenu.addMenu(addVarMenu)
+
     def onCreateVariant(self, origin, entity, department, taskName, existingTasks):
         
         #-----------------------------------------------------------------------------------#
@@ -340,7 +215,7 @@ class Prism_Daisy_Pipe_Functions(object):
         # Create the task with the same name and variant increment                          #
         #-----------------------------------------------------------------------------------#
         
-        #Check existing tasks and determine the right name
+        # Check existing tasks and determine the right name
         if f"{taskName}_02" not in existingTasks:
             varTaskName = f"{taskName}_var02"
         else:
@@ -361,11 +236,24 @@ class Prism_Daisy_Pipe_Functions(object):
         origin.refreshTasks()
         return path
 
+
+
+
+
+
+
     # if returns true, the plugin will be loaded by Prism
     @err_catcher(name=__name__)
     def isActive(self):
         return True
     
+
+
+
+
+
+
+
     def importUsdPackages(self):
 
         try:
