dce78508782fa906135ebd2ed3055c1dc4b17f80
Thomas Rubio <thomasrub@etik.com>
2026-09-08 17:36:50 +0200

    launch usdcat in powershell to convert USD format triggered by a button in Prism

--- Daisy_Pipe/Scripts/DaisyTools/core/command_launcher.py
+++ Daisy_Pipe/Scripts/DaisyTools/core/command_launcher.py
@@ -31,9 +31,15 @@ from typing import Any
 
 
 class Command_launcher(object):
-    def __init__(self, core: Any, plugin: Any):
+    def __init__(self, core: Any, plugin: Any = None) -> None:
         self.core = core
         self.plugin = plugin
+        self.project_path = self.core.projectPath
+        self.project_path = self.project_path.replace("\\", "/")
+
+        # importe path.json
+        with open(f'{self.project_path}00_Pipeline/Plugins/Daisy_Pipe/Scripts/DaisyTools/lib/path.json', 'r') as file:
+            self.jsonPath = json.load(file)
         
     ######################################################################################################################################
     ########################################################### SET METHODS ##############################################################
@@ -43,22 +49,16 @@ class Command_launcher(object):
 
         #---------------------------------------------------------------------------------------------------#
         # launch the create_asset.py script with hython in powershell                                       #
+        #                                                                                                   #
         # asset name : name of the asset to be processed, it is passed in the command line to hython        #
         # path : path of the asset to be processed, it is passed in the command line to hython              #
         # project path : path of the project, it is passed in the command line to hython                    #
         #---------------------------------------------------------------------------------------------------#
 
-        project_path = self.core.projectPath
-        project_path = project_path.replace("\\", "/")
-
-        # importe path.json to get the path of hython.exe
-        with open(f'{project_path}00_Pipeline/Plugins/Daisy_Pipe/Scripts/DaisyTools/lib/path.json', 'r') as file:
-            jsonPath = json.load(file)
-
-        hython_path = jsonPath["software"]["hython"]
+        hython_path = self.jsonPath["software"]["hython"]
 
         # path to the create_asset.py script to be launched with hython
-        python_file_path = f"{project_path}00_Pipeline/Plugins/Daisy_Pipe/Scripts/DaisyTools/core/create_asset.py"
+        python_file_path = f"{self.project_path}00_Pipeline/Plugins/Daisy_Pipe/Scripts/DaisyTools/core/create_asset.py"
 
         asset_name = str(asset_name)
         to_hython_path = "cd \'" + hython_path.replace("/hython.exe", "") + "\'"
@@ -69,7 +69,38 @@ class Command_launcher(object):
         path_to_asset = self.core.paths.getEntityPath(entity=current_entity)
 
         # create command line to launch hython with the create_asset.py script and pass the asset name and info as arguments
-        command_line = f"powershell.exe \"{to_hython_path}\" ; ./hython.exe \"{python_file_path}\" --assetName \'{asset_name}\' --path '{path_to_asset}' --assetPath '{asset_path}' --projectPath '{project_path}' --packed '{packed}'"
+        command_line = f"powershell.exe \"{to_hython_path}\" ; ./hython.exe \"{python_file_path}\" --assetName \'{asset_name}\' --path '{path_to_asset}' --assetPath '{asset_path}' --projectPath '{self.project_path}' --packed '{packed}'"
 
         # launch command line in powershell
         subprocess.Popen(command_line)
+
+    def convert_usd_format(self, entity: Any, usd_in: str = "usd", usd_out: str = "usda") -> None:
+        #---------------------------------------------------------------------------------------------------#
+        # launch usdcat in powershell to convert USD format                                                 #
+        #                                                                                                   #       
+        # entity : the entity for which to convert USD format                                               #
+        # usd_in : the input USD format                                                                     #
+        # usd_out : the output USD format                                                                   #
+        #---------------------------------------------------------------------------------------------------#
+
+        usdcat_path = self.jsonPath["software"]["usdcat"]
+        usdcat_path_to_del = usdcat_path.split("/")[-1]
+        usdcat_path = usdcat_path.replace(f"/{usdcat_path_to_del}", "")
+
+        input_path = None
+        print(f"{input_path = }")
+        output_path = None
+        print(f"{output_path = }")
+        to_usdcat_path = f"cd \'{usdcat_path}\'"
+        print(f"{to_usdcat_path = }")
+
+        # create command line to convert USD format using usdcat
+        if usd_out == "usd":
+            command_line = f"powershell.exe \"{to_usdcat_path}\" ; ./usdcat --out \"{output_path}\" --usdFormat \"{usd_out}\" \"{input_path}.{usd_out}\""
+        else:
+            command_line = f"powershell.exe \"{to_usdcat_path}\" ; ./usdcat --out \"{output_path}\" \"{input_path}\""
+        print(f"{command_line = }")
+
+        # launch command line in powershell
+        subprocess.Popen(command_line)
+
